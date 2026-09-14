db7698f7f408e6f8a0dd930c2fe7414e083394f4
Shrinith-S <shrinith.s.sanil@gmail.com>
2026-09-14 20:28:55 +0530

    DTOs, Validation, and Manual Mapping || Custom Exceptions and Global Exception Handling || Repositories

---
+++ CustomerSupport.Application/Interfaces/Repositories/ITicketPriorityRepository.cs
@@ -0,0 +1,11 @@
+﻿using CustomerSupport.Domain.Entities;
+using CustomerSupport.Domain.Enums;
+namespace CustomerSupport.Application.Interfaces.Repositories
+{
+    public interface ITicketPriorityRepository
+    {
+        Task<IReadOnlyCollection<TicketPriority>> GetAllAsync(bool activeOnly);
+        Task<TicketPriority?> GetByIdAsync(TicketPriorityType id);
+    }
+}
+
